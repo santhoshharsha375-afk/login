d7d27eb3e6496a368cc47b035b48ce3391cc34e0
santhoshharsha375-afk <santhoshharsha375@gmail.com>
2026-01-31 18:34:26 +0530

    Initial commit - React app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import Login from "./Login.jsx";
+
+function App() {
+  return <Login />;
+}
+
+export default App;
+
---
+++ src/Login.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      alert("Fill all fields");
+      return;
+    }
+
+    if (password.length < 6) {
+      alert("Password must be at least 6 characters");
+      return;
+    }
+
+    alert("Login Successful");
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br /><br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
